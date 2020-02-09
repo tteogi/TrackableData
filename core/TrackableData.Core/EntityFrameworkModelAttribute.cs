57fc707e462f711c601c8625a420fe12ec435754
tteogi <tteogi@daum.net>
2020-02-09 15:11:06 +0900

    Remove IEntityFrameworkModel interface, Add EntityFrameworkPropertyAttribute and EntityFrameworkModelAttribute

--- core/TrackableData.Core/EntityFrameworkModelAttribute.cs
+++ core/TrackableData.Core/EntityFrameworkModelAttribute.cs
@@ -1,12 +1,58 @@
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace TrackableData
 {
     public class EntityFrameworkModelAttribute : Attribute
     {
-        public EntityFrameworkModelAttribute(string databaseAttribute)
-        {
+        public string[] Parameters;
 
+        public EntityFrameworkModelAttribute(params string[] parameters)
+        {
+            Parameters = parameters;
+        }
+
+        public string this[string parameter]
+        {
+            get
+            {
+                if (parameter.EndsWith(":"))
+                {
+                    // as property
+                    var p = Parameters.FirstOrDefault(x => x.StartsWith(parameter));
+                    return p?.Substring(parameter.Length);
+                }
+                else
+                {
+                    // as flag
+                    return Parameters.Any(p => p == parameter) ? "true" : null;
+                }
+            }
+        }
+
+        public static string GetParameter(ICustomAttributeProvider provider, string parameter)
+        {
+            if (parameter.EndsWith(":"))
+            {
+                // as property
+                foreach (var property in provider.GetCustomAttributes(false).OfType<TrackablePropertyAttribute>())
+                {
+                    var p = property.Parameters.FirstOrDefault(x => x.StartsWith(parameter));
+                    if (p != null)
+                        return p.Substring(parameter.Length);
+                }
+            }
+            else
+            {
+                // as flag
+                foreach (var property in provider.GetCustomAttributes(false).OfType<TrackablePropertyAttribute>())
+                {
+                    if (property.Parameters.Any(p => p == parameter))
+                        return "true";
+                }
+            }
+            return null;
         }
     }
 }
